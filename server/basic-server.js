fa0546ece93e2d841a3284015564512fe8595b91
doitlikepruett <doitlikepruett@gmail.com>
2016-07-13 20:12:52 -0700

    added mongo link

--- server/basic-server.js
+++ server/basic-server.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var app = express();
 
 //connect to mongo db named 'suggestionDB'
-mongoose.connect('mongodb://localhost/suggestionDB');
+mongoose.connect('mongodb://localhost/suggestionDB' || 'mongodb://heroku_g1sph5gs:kabe1vjced31m77ek7np2dfus1@ds037447.mlab.com:37447/heroku_g1sph5gs');
 
 //add middleware and routing
 require('./middleware.js')(app, express);
@@ -14,9 +14,11 @@ var port = process.env.PORT || 5555;
 
 app.use(express.static(__dirname + '/../client'));
 
-app.listen(port, function(){
+app.listen(port, function() {
   console.log('Server listening on ' + port)
 });
 
 module.exports  = app
 
+
+// MONGODB_URI: mongodb://heroku_g1sph5gs:kabe1vjced31m77ek7np2dfus1@ds037447.mlab.com:37447/heroku_g1sph5gs
